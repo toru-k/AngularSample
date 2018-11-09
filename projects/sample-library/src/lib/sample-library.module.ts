c6a2cd4aa83aba0115a4005eb6e376c5ca5b196a
toru-k <toru_kobayashi@three-hands-works.com>
2018-11-09 15:07:58 +0900

    library作成 ng g library sampleLibrary

---
+++ projects/sample-library/src/lib/sample-library.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { SampleLibraryComponent } from './sample-library.component';
+
+@NgModule({
+  declarations: [SampleLibraryComponent],
+  imports: [
+  ],
+  exports: [SampleLibraryComponent]
+})
+export class SampleLibraryModule { }
