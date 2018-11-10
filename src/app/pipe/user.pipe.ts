c1c8abb48fa04bf11d7644833e0145d77777bc46
toru-k <toru_kobayashi@three-hands-works.com>
2018-11-10 14:05:09 +0900

    Pipeサンプル実装

--- src/app/pipe/user.pipe.ts
+++ src/app/pipe/user.pipe.ts
@@ -1,12 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
 
 @Pipe({
-  name: 'user'
+  name: 'date'
 })
 export class UserPipe implements PipeTransform {
-
-  transform(value: any, args?: any): any {
-    return null;
+  transform(date: string, format: string): string {
+    return moment().format(format);
   }
-
 }
