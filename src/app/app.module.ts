75652fa28f50f9163b188899a58e44a5cb8010dd
toru-k <toru_kobayashi@three-hands-works.com>
2018-11-10 17:00:50 +0900

    性別表示のpipe追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,18 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './component/user/users/users.component';
 import { UserPipe } from './pipe/user.pipe';
 import { UserDirective } from './directive/user.directive';
+import { SexPipe } from './pipe/sex.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     UserPipe,
-    UserDirective
+    UserDirective,
+    SexPipe
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
