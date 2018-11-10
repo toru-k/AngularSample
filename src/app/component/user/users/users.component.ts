05f2fa1bc041c9378e3415568993b46edea2dc83
toru-k <toru_kobayashi@three-hands-works.com>
2018-11-10 15:43:11 +0900

    enumに関数を定義するサンプル

--- src/app/component/user/users/users.component.ts
+++ src/app/component/user/users/users.component.ts
@@ -11,30 +11,12 @@ export class UsersComponent implements OnInit {
 
   ngOnInit() {
     const enum1 = Sample.Sample1;
-    console.log(this.sampleMethod(enum1));
+    console.log(Sample.sampleMethod(enum1));
     console.log(enum1);
+    console.log(Sample[enum1]);
 
     const enum2 = Sample2.Sample3;
     console.log(enum2);
     console.log(Sample2[enum2]);
-  }
-
-  sampleMethod(sample: Sample): string {
-    let message = '';
-    switch (sample) {
-      case Sample.Sample1:
-        message = 'It\'s Sample1.';
-        break;
-      case Sample.Sample2:
-        message = 'It\'s Sample2.';
-        break;
-      case Sample.Sample3:
-        message = 'It\'s Sample3.';
-        break;
-      default:
-        message = 'not found.';
-        break;
-    }
-    return message;
   }
 }
--- src/app/enum/sample.enum.ts
+++ src/app/enum/sample.enum.ts
@@ -5,6 +5,28 @@ enum Sample {
   Sample3
 }
 
+// enumに関数を定義する。
+namespace Sample {
+  export function sampleMethod(sample: Sample): string {
+    let message = '';
+    switch (sample) {
+      case Sample.Sample1:
+        message = 'It\'s Sample1.';
+        break;
+      case Sample.Sample2:
+        message = 'It\'s Sample2.';
+        break;
+      case Sample.Sample3:
+        message = 'It\'s Sample3.';
+        break;
+      default:
+        message = 'not found.';
+        break;
+    }
+    return message;
+  }
+}
+
 enum Sample2 {
   Sample1 = 'enum Sample1',
   Sample2 = 'enum Sample2',
