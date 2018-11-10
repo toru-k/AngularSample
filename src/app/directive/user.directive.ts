8a50699185bb10b9df52017fb62e8bf96c5429e1
toru-k <toru_kobayashi@three-hands-works.com>
2018-11-10 12:56:38 +0900

    Directiveの実装と使用方法

--- src/app/directive/user.directive.ts
+++ src/app/directive/user.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appUser]'
 })
 export class UserDirective {
-
-  constructor() { }
-
+  constructor(el: ElementRef) {
+    // Directiveの実装サンプル（下線を追加）
+    const element = el.nativeElement as HTMLElement;
+    element.style.textDecoration = 'underline';
+  }
 }
