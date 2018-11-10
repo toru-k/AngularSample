22ac144b7af70c83c6e55bd5940657c4e661b18c
toru-k <toru_kobayashi@three-hands-works.com>
2018-11-10 15:31:56 +0900

    enumサンプル

--- src/app/component/user/users/users.component.ts
+++ src/app/component/user/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Sample, Sample2 } from 'src/app/enum/sample.enum';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    const enum1 = Sample.Sample1;
+    console.log(this.sampleMethod(enum1));
+    console.log(enum1);
+
+    const enum2 = Sample2.Sample3;
+    console.log(enum2);
+    console.log(Sample2[enum2]);
   }
 
+  sampleMethod(sample: Sample): string {
+    let message = '';
+    switch (sample) {
+      case Sample.Sample1:
+        message = 'It\'s Sample1.';
+        break;
+      case Sample.Sample2:
+        message = 'It\'s Sample2.';
+        break;
+      case Sample.Sample3:
+        message = 'It\'s Sample3.';
+        break;
+      default:
+        message = 'not found.';
+        break;
+    }
+    return message;
+  }
 }
--- src/app/enum/sample.enum.ts
+++ src/app/enum/sample.enum.ts
@@ -1,2 +1,12 @@
-export enum Sample {
+export { Sample, Sample2 };
+enum Sample {
+  Sample1,
+  Sample2,
+  Sample3
+}
+
+enum Sample2 {
+  Sample1 = 'enum Sample1',
+  Sample2 = 'enum Sample2',
+  Sample3 = 'enum Sample3'
 }
